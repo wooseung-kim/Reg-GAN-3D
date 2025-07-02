1d96c3e3d29be453d6c1a0eb8de5cbcf2b089270
Wooseung Kim <129208752+wooseung-kim@users.noreply.github.com>
2025-07-02 18:24:24 +0900

    Update layers.py in 3D

--- trainer/layers.py
+++ trainer/layers.py
@@ -4,20 +4,17 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
+# Configuration for 3D operations
 scale_eval = False
-
 alpha = 0.02
 beta = 0.00002
 
 resnet_n_blocks = 1
 
-norm_layer = partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
+# Use 3D instance normalization by default
+norm_layer = partial(nn.InstanceNorm3d, affine=False, track_running_stats=False)
 align_corners = False
-up_sample_mode = 'bilinear'
-
-
-def custom_init(m):
-    m.data.normal_(0.0, alpha)
+up_sample_mode = 'trilinear'
 
 
 def get_init_function(activation, init_function, **kwargs):
@@ -68,33 +65,31 @@ def get_activation(activation, **kwargs):
         return None
 
 
-class Conv(torch.nn.Module):
-    """Defines a basic convolution layer.
-    The general structure is as follow:
-
-    Conv -> Norm (optional) -> Activation -----------> + --> Output
-                                         |            ^
-                                         |__ResBlcok__| (optional)
-    """
-
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=True, activation='relu',
-                 init_func='kaiming', use_norm=False, use_resnet=False, **kwargs):
+class Conv(nn.Module):
+    """Basic 3D convolution -> Norm -> Activation -> (optional ResNet)"""
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding,
+                 bias=True, activation='relu', init_func='kaiming', use_norm=False,
+                 use_resnet=False, **kwargs):
         super(Conv, self).__init__()
-        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias)
-        self.resnet_block = ResnetTransformer(out_channels, resnet_n_blocks, init_func) if use_resnet else None
+        self.conv = nn.Conv3d(in_channels, out_channels,
+                              kernel_size=kernel_size, stride=stride,
+                              padding=padding, bias=bias)
         self.norm = norm_layer(out_channels) if use_norm else None
         self.activation = get_activation(activation, **kwargs)
-        # Initialize the weights
+        self.resnet_block = ResnetTransformer(out_channels, resnet_n_blocks, init_func) if use_resnet else None
+
+        # Initialize weights
         init_ = get_init_function(activation, init_func)
-        init_(self.conv2d.weight)
-        if self.conv2d.bias is not None:
-            self.conv2d.bias.data.zero_()
-        if self.norm is not None and isinstance(self.norm, nn.BatchNorm2d):
-            nn.init.normal_(self.norm.weight.data, 0.0, 1.0)
-            nn.init.constant_(self.norm.bias.data, 0.0)
+        init_(self.conv.weight)
+        if self.conv.bias is not None:
+            nn.init.constant_(self.conv.bias, 0)
+        if self.norm is not None:
+            # InstanceNorm3d has weight and bias
+            nn.init.normal_(self.norm.weight, 1.0, 0.02)
+            nn.init.constant_(self.norm.bias, 0.0)
 
     def forward(self, x):
-        x = self.conv2d(x)
+        x = self.conv(x)
         if self.norm is not None:
             x = self.norm(x)
         if self.activation is not None:
@@ -104,135 +99,140 @@ class Conv(torch.nn.Module):
         return x
 
 
-class UpBlock(torch.nn.Module):
-    def __init__(self, nc_down_stream, nc_skip_stream, nc_out, kernel_size, stride, padding, bias=True,
-                 activation='relu',
-                 init_func='kaiming', use_norm=False, refine=False, use_resnet=False, use_add=False,
-                 use_attention=False, **kwargs):
+class UpBlock(nn.Module):
+    def __init__(self, nc_down, nc_skip, nc_out, kernel_size, stride, padding,
+                 bias=True, activation='relu', init_func='kaiming', use_norm=False,
+                 refine=False, use_resnet=False, use_add=False, use_attention=False, **kwargs):
         super(UpBlock, self).__init__()
-        if 'nc_inner' in kwargs:
-            nc_inner = kwargs['nc_inner']
-        else:
-            nc_inner = nc_out
-        self.conv_0 = Conv(nc_down_stream + nc_skip_stream, nc_inner, kernel_size=kernel_size, stride=stride,
-                           padding=padding, bias=bias, activation=activation, init_func=init_func, use_norm=use_norm,
-                           use_resnet=use_resnet, **kwargs)
-        self.conv_1 = None
-        if refine:
-            self.conv_1 = Conv(nc_inner, nc_inner, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias,
-                               activation=activation, init_func=init_func, use_norm=use_norm, use_resnet=use_resnet,
-                               **kwargs)
-        self.use_attention = use_attention
-        if self.use_attention:
-            self.attention_gate = AttentionGate(nc_down_stream, nc_skip_stream, nc_inner, use_norm=True,
-                                                init_func=init_func)
-        self.up_conv = Conv(nc_inner, nc_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias,
-                            activation=activation, init_func=init_func, use_norm=use_norm, use_resnet=False, **kwargs)
-        self.use_add = use_add
-        if self.use_add:
-            self.output = Conv(nc_out, 2, kernel_size=1, stride=1, padding=0, bias=bias, activation=None,
-                               init_func='zeros',
-                               use_norm=False, use_resnet=False)
+        nc_inner = kwargs.get('nc_inner', nc_out)
 
-    def forward(self, down_stream, skip_stream):
-        down_stream_size = down_stream.size()
-        skip_stream_size = skip_stream.size()
+        self.conv0 = Conv(nc_down + nc_skip, nc_inner,
+                          kernel_size=kernel_size, stride=stride, padding=padding,
+                          bias=bias, activation=activation,
+                          init_func=init_func, use_norm=use_norm,
+                          use_resnet=use_resnet, **kwargs)
+        self.conv1 = Conv(nc_inner, nc_inner,
+                          kernel_size=kernel_size, stride=stride, padding=padding,
+                          bias=bias, activation=activation,
+                          init_func=init_func, use_norm=use_norm,
+                          use_resnet=use_resnet, **kwargs) if refine else None
+        self.use_attention = use_attention
+        if use_attention:
+            self.attention_gate = AttentionGate(nc_down, nc_skip, nc_inner,
+                                                use_norm=use_norm, init_func=init_func)
+        self.up_conv = Conv(nc_inner, nc_out,
+                            kernel_size=kernel_size, stride=stride, padding=padding,
+                            bias=bias, activation=activation,
+                            init_func=init_func, use_norm=use_norm, **kwargs)
+        self.use_add = use_add
+        if use_add:
+            self.output = Conv(nc_out, nc_out,
+                               kernel_size=1, stride=1, padding=0,
+                               bias=bias, activation=None,
+                               init_func='zeros', use_norm=False)
+
+    def forward(self, down, skip):
         if self.use_attention:
-            skip_stream = self.attention_gate(down_stream, skip_stream)
-        if down_stream_size[2] != skip_stream_size[2] or down_stream_size[3] != skip_stream_size[3]:
-            down_stream = F.interpolate(down_stream, (skip_stream_size[2], skip_stream_size[3]),
-                                        mode=up_sample_mode, align_corners=align_corners)
-        x = torch.cat([down_stream, skip_stream], 1)
-        x = self.conv_0(x)
-        if self.conv_1 is not None:
-            x = self.conv_1(x)
+            skip = self.attention_gate(down, skip)
+        if down.shape[2:] != skip.shape[2:]:
+            down = F.interpolate(down, size=skip.shape[2:],
+                                 mode=up_sample_mode, align_corners=align_corners)
+        x = torch.cat([down, skip], dim=1)
+        x = self.conv0(x)
+        if self.conv1 is not None:
+            x = self.conv1(x)
         if self.use_add:
-            x = self.output(x) + down_stream
+            x = self.output(x) + down
         else:
             x = self.up_conv(x)
         return x
 
 
-class DownBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=False, activation='relu',
-                 init_func='kaiming', use_norm=False, use_resnet=False, skip=True, refine=False, pool=True,
-                 pool_size=2, **kwargs):
+class DownBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding,
+                 bias=False, activation='relu', init_func='kaiming',
+                 use_norm=False, use_resnet=False, skip=True,
+                 refine=False, pool=True, pool_size=2, **kwargs):
         super(DownBlock, self).__init__()
-        self.conv_0 = Conv(in_channels, out_channels, kernel_size, stride, padding, bias=bias,
-                           activation=activation, init_func=init_func, use_norm=use_norm, callback=None,
-                           use_resnet=use_resnet, **kwargs)
-        self.conv_1 = None
-        if refine:
-            self.conv_1 = Conv(out_channels, out_channels, kernel_size, stride, padding, bias=bias,
-                               activation=activation, init_func=init_func, use_norm=use_norm, callback=None,
-                               use_resnet=use_resnet, **kwargs)
+        self.conv0 = Conv(in_channels, out_channels,
+                          kernel_size=kernel_size, stride=stride, padding=padding,
+                          bias=bias, activation=activation,
+                          init_func=init_func, use_norm=use_norm,
+                          use_resnet=use_resnet, **kwargs)
+        self.conv1 = Conv(out_channels, out_channels,
+                          kernel_size=kernel_size, stride=stride, padding=padding,
+                          bias=bias, activation=activation,
+                          init_func=init_func, use_norm=use_norm,
+                          use_resnet=use_resnet, **kwargs) if refine else None
         self.skip = skip
-        self.pool = None
-        if pool:
-            self.pool = nn.MaxPool2d(kernel_size=pool_size)
+        self.pool = nn.MaxPool3d(pool_size) if pool else None
 
     def forward(self, x):
-        x = skip = self.conv_0(x)
-        if self.conv_1 is not None:
-            x = skip = self.conv_1(x)
+        x = self.conv0(x)
+        skip = x
+        if self.conv1 is not None:
+            x = self.conv1(x)
+            skip = x
         if self.pool is not None:
             x = self.pool(x)
-        if self.skip:
-            return x, skip
-        else:
-            return x
+        return (x, skip) if self.skip else x
 
 
-class AttentionGate(torch.nn.Module):
-    def __init__(self, nc_g, nc_x, nc_inner, use_norm=False, init_func='kaiming', mask_channel_wise=False):
+class AttentionGate(nn.Module):
+    def __init__(self, nc_g, nc_x, nc_inner, use_norm=False,
+                 init_func='kaiming', mask_channel_wise=False):
         super(AttentionGate, self).__init__()
-        self.conv_g = Conv(nc_g, nc_inner, 1, 1, 0, bias=True, activation=None, init_func=init_func,
-                           use_norm=use_norm, use_resnet=False)
-        self.conv_x = Conv(nc_x, nc_inner, 1, 1, 0, bias=False, activation=None, init_func=init_func,
-                           use_norm=use_norm, use_resnet=False)
+        self.conv_g = Conv(nc_g, nc_inner,
+                           kernel_size=1, stride=1, padding=0,
+                           bias=True, activation=None,
+                           init_func=init_func, use_norm=use_norm)
+        self.conv_x = Conv(nc_x, nc_inner,
+                           kernel_size=1, stride=1, padding=0,
+                           bias=False, activation=None,
+                           init_func=init_func, use_norm=use_norm)
         self.residual = nn.ReLU(inplace=True)
         self.mask_channel_wise = mask_channel_wise
-        self.attention_map = Conv(nc_inner, nc_x if mask_channel_wise else 1, 1, 1, 0, bias=True, activation='sigmoid',
-                                  init_function=init_func, use_norm=use_norm, use_resnet=False)
+        output_ch = nc_x if mask_channel_wise else 1
+        self.att_map = Conv(nc_inner, output_ch,
+                            kernel_size=1, stride=1, padding=0,
+                            bias=True, activation='sigmoid',
+                            init_func=init_func, use_norm=use_norm)
 
     def forward(self, g, x):
-        x_size = x.size()
-        g_size = g.size()
-        x_resized = x
         g_c = self.conv_g(g)
-        x_c = self.conv_x(x_resized)
-        if x_c.size(2) != g_size[2] and x_c.size(3) != g_size[3]:
-            x_c = F.interpolate(x_c, (g_size[2], g_size[3]), mode=up_sample_mode, align_corners=align_corners)
+        x_c = self.conv_x(x)
+        if g_c.shape[2:] != x_c.shape[2:]:
+            x_c = F.interpolate(x_c, size=g_c.shape[2:],
+                                 mode=up_sample_mode, align_corners=align_corners)
         combined = self.residual(g_c + x_c)
-        alpha = self.attention_map(combined)
+        alpha = self.att_map(combined)
         if not self.mask_channel_wise:
-            alpha = alpha.repeat(1, x_size[1], 1, 1)
-        alpha_size = alpha.size()
-        if alpha_size[2] != x_size[2] and alpha_size[3] != x_size[3]:
-            alpha = F.interpolate(x, (x_size[2], x_size[3]), mode=up_sample_mode, align_corners=align_corners)
+            alpha = alpha.expand(-1, x.shape[1], -1, -1, -1)
+        if alpha.shape[2:] != x.shape[2:]:
+            alpha = F.interpolate(alpha, size=x.shape[2:],
+                                  mode=up_sample_mode, align_corners=align_corners)
         return alpha * x
 
 
-class ResnetTransformer(torch.nn.Module):
+class ResnetTransformer(nn.Module):
     def __init__(self, dim, n_blocks, init_func):
         super(ResnetTransformer, self).__init__()
-        model = []
-        for i in range(n_blocks):  # add ResNet blocks
-            model += [
-                ResnetBlock(dim, padding_type='reflect', norm_layer=norm_layer, use_dropout=False,
-                            use_bias=True)]
-        self.model = nn.Sequential(*model)
-
+        layers = []
+        for _ in range(n_blocks):
+            layers.append(ResnetBlock3D(dim, padding_type='reflect',
+                                        norm_layer=norm_layer, use_dropout=False,
+                                        use_bias=True))
+        self.model = nn.Sequential(*layers)
         init_ = get_init_function('relu', init_func)
 
         def init_weights(m):
-            if type(m) == nn.Conv2d:
+            if isinstance(m, nn.Conv3d):
                 init_(m.weight)
                 if m.bias is not None:
-                    m.bias.data.zero_()
-            if type(m) == nn.BatchNorm2d:
-                nn.init.normal_(m.weight.data, 1.0, 0.02)
-                nn.init.constant_(m.bias.data, 0.0)
+                    nn.init.constant_(m.bias, 0)
+            if isinstance(m, nn.InstanceNorm3d):
+                nn.init.normal_(m.weight, 1.0, 0.02)
+                nn.init.constant_(m.bias, 0.0)
 
         self.model.apply(init_weights)
 
@@ -240,61 +240,43 @@ class ResnetTransformer(torch.nn.Module):
         return self.model(x)
 
 
-class ResnetBlock(nn.Module):
-    """Define a Resnet block"""
-
+class ResnetBlock3D(nn.Module):
+    """Define a 3D Resnet block"""
     def __init__(self, dim, padding_type, norm_layer, use_dropout, use_bias):
-        """Initialize the Resnet block
+        super(ResnetBlock3D, self).__init__()
+        self.conv_block = self.build_conv_block(dim, padding_type,
+                                                norm_layer, use_dropout,
+                                                use_bias)
 
-        A resnet block is a conv block with skip connections
-        We construct a conv block with build_conv_block function,
-        and implement skip connections in <forward> function.
-        Original Resnet paper: https://arxiv.org/pdf/1512.03385.pdf
-        """
-        super(ResnetBlock, self).__init__()
-        self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, use_dropout, use_bias)
-
-    def build_conv_block(self, dim, padding_type, norm_layer, use_dropout, use_bias):
-        """Construct a convolutional block.
-
-        Parameters:
-            dim (int)           -- the number of channels in the conv layer.
-            padding_type (str)  -- the name of padding layer: reflect | replicate | zero
-            norm_layer          -- normalization layer
-            use_dropout (bool)  -- if use dropout layers.
-            use_bias (bool)     -- if the conv layer uses bias or not
-
-        Returns a conv block (with a conv layer, a normalization layer, and a non-linearity layer (ReLU))
-        """
+    def build_conv_block(self, dim, padding_type, norm_layer,
+                         use_dropout, use_bias):
         conv_block = []
-        p = 0
+        # padding for 3D
         if padding_type == 'reflect':
-            conv_block += [nn.ReflectionPad2d(1)]
+            conv_block.append(nn.ReflectionPad3d(1))
         elif padding_type == 'replicate':
-            conv_block += [nn.ReplicationPad2d(1)]
+            conv_block.append(nn.ReplicationPad3d(1))
         elif padding_type == 'zero':
-            p = 1
+            pad = 1
         else:
-            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
-
-        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim), nn.ReLU(True)]
+            raise NotImplementedError(f'padding [{padding_type}] not implemented')
+        # first conv
+        conv_block += [nn.Conv3d(dim, dim, kernel_size=3,
+                                 padding=0 if padding_type != 'zero' else pad,
+                                 bias=use_bias), norm_layer(dim), nn.ReLU(inplace=True)]
         if use_dropout:
-            conv_block += [nn.Dropout(0.5)]
-
-        p = 0
+            conv_block.append(nn.Dropout3d(0.5))
+        # second conv
         if padding_type == 'reflect':
-            conv_block += [nn.ReflectionPad2d(1)]
+            conv_block.append(nn.ReflectionPad3d(1))
         elif padding_type == 'replicate':
-            conv_block += [nn.ReplicationPad2d(1)]
+            conv_block.append(nn.ReplicationPad3d(1))
         elif padding_type == 'zero':
-            p = 1
-        else:
-            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
-        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim)]
-
+            pad = 1
+        conv_block += [nn.Conv3d(dim, dim, kernel_size=3,
+                                 padding=0 if padding_type != 'zero' else pad,
+                                 bias=use_bias), norm_layer(dim)]
         return nn.Sequential(*conv_block)
 
     def forward(self, x):
-        """Forward function (with skip connections)"""
-        out = x + self.conv_block(x)  # add skip connections
-        return out
+        return x + self.conv_block(x)  # skip connection
